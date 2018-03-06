2c02830bc3362091d9754aabb02034d362bd3753
Erik Laurin <guslauer@student.gu.se>
2018-03-06 21:04:07 +0100

    updated code: removed reverse command and checks that user input is valid

--- safety_check/Example.cpp
+++ safety_check/Example.cpp
@@ -15,47 +15,46 @@ int main(int /*argc*/, char ** /*argv*/) {
         if (envelope.dataType() == opendlv::proxy::GroundSteeringReading::ID()) {
             opendlv::proxy::GroundSteeringReading receivedMsg = cluon::extractMessage<opendlv::proxy::GroundSteeringReading>(
                     std::move(envelope));
-            std::cout << "Sent a message for ground steering to " << receivedMsg.steeringAngle() << "." << std::endl;
+            std::cout << "Request for steering angle: " << receivedMsg.steeringAngle() << " sent." << std::endl;
         } else if (envelope.dataType() == opendlv::proxy::PedalPositionReading::ID()) {
             opendlv::proxy::PedalPositionReading receivedMsg = cluon::extractMessage<opendlv::proxy::PedalPositionReading>(
                     std::move(envelope));
-            std::cout << "Sent a message for pedal position to " << receivedMsg.percent() << "." << std::endl;
+            std::cout << "Request for pedal position: " << receivedMsg.percent() << " sent." << std::endl;
         }
     });
 
     if (od4.isRunning() == 0) {
-        std::cout << "ERROR: No od4 running!!!" << std::endl;
+        std::cout << "ERROR: No OD4 is running!" << std::endl;
         return -1;
     } else {
-        std::cout << "OD4 is running and you're good to go!" << std::endl;
+        std::cout << "OD4 is running and you're good to go!\n" << std::endl;
     }
 
     std::cout << "Commands: \n"
             "w - move forward \n"
-            "s - move backwards \n"
-            "d - turn right \n"
             "a - turn left \n"
-            "e - quit \n"
+            "d - turn right \n"
+            "any other key - stop \n\n"
             "v - set speed \n"
-            "b - set angle \n"
-
-            "any other key - stop \n" << std::endl;
+            "b - set angle \n\n"
+            "e - exit \n"
+              << std::endl;
 
     opendlv::proxy::GroundSteeringReading msgSteering;
     opendlv::proxy::PedalPositionReading msgPedal;
 
-    float speed = 0.4;
+    float speed = 0.3;
     float steeringAngle = 15.0;
     bool exit = false;
     char command;
 
-    while (od4.isRunning() && !exit){
+    while (od4.isRunning() && !exit) {
         std::cin >> command;
 
         switch (command) {
 
             case 'w':
-                std::cout << "Forwards" << std::endl;
+                std::cout << "Forward" << std::endl;
                 msgSteering.steeringAngle(0.0);
                 od4.send(msgSteering);
                 msgPedal.percent(speed);
@@ -75,31 +74,37 @@ int main(int /*argc*/, char ** /*argv*/) {
                 msgPedal.percent(speed);
                 od4.send(msgPedal);
                 break;
-            case 's':
-                std::cout << "Backwards" << std::endl;
-                msgSteering.steeringAngle(0.0);
-                od4.send(msgSteering);
-                msgPedal.percent(-speed);
-                od4.send(msgPedal);
-                break;
             case 'e':
+                std::cout << "Exits.. " << std::endl;
                 exit = true;
+                break;
             case 'v':
                 msgPedal.percent(0);
                 od4.send(msgPedal);
-                std::cout << "Set speed, enter a value" << std::endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+                std::cout << "Speed - enter a value in the range 0.15 to 1.00: " << std::endl;
                 std::cin >> speed;
-                std::cout << "Speed set to: " << speed << std::endl;
+                while (speed < 0.15 || speed > 1.00) {
+                    std::cout << "ERROR: the value must be in the range 0.15 to 1.00! Try again: " << std::endl;
+                    std::cin >> speed;
+                }
+                std::cout << "Speed successfully set to: " << speed << std::endl;
                 break;
             case 'b':
                 msgPedal.percent(0);
                 od4.send(msgPedal);
-                std::cout << "Set angle, enter a value" << std::endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                std::cout << "Angle - enter a value in the range 0 to 20: " << std::endl;
                 std::cin >> steeringAngle;
-                std::cout << "Angle set to: " << steeringAngle << std::endl;
+                while (steeringAngle < 0 || steeringAngle > 20) {
+                    std::cout << "ERROR: the value must be in the range 0 to 20! Try again: " << std::endl;
+                    std::cin >> steeringAngle;
+                }
+                std::cout << "Steering angle successfully set to: " << steeringAngle << std::endl;
                 break;
             default:
-                std::cout << "stop!" << std::endl;
+                std::cout << "Stop!" << std::endl;
                 msgSteering.steeringAngle(0.0);
                 od4.send(msgSteering);
                 msgPedal.percent(0);
