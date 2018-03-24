8e8f671eb334f2029f99056d20c3dcd2a002951a
Isabelle Törnqvist <gustornqis@student.gu.se>
2018-03-24 15:44:24 +0100

    added comments etc

--- web-interface/NavKeys.js
+++ web-interface/NavKeys.js
@@ -1,6 +1,11 @@
+/*!
+ * NavKeys.js
+ * Version 1.0
+ *
+ * Used to mark(grey) pressed navigation keys in web-interface
+ */
 
-
-//ASCII codes
+//ASCII codes for arrow keys
 var key_left = 37;
 var key_forward = 38;
 var key_right = 39;
@@ -9,10 +14,11 @@ function initializeNavKeys() {
 
     var keyState = [false, false, false];
 
-    //if (any) arrow key is pressed down
+    //when arrow key is pressed down
     $(document).keydown(function (e) {
         if (e.keyCode == key_left){
             keyState[0] = true;
+            document.getElementById("LeftBtn").style.backgroundColor = "gray";
         }
         if (e.keyCode == key_forward){
             keyState[1] = true;
@@ -20,12 +26,15 @@ function initializeNavKeys() {
         }
         if (e.keyCode == key_right){
             keyState[2] = true;
+            document.getElementById("RightBtn").style.backgroundColor = "gray";
         }
     });
 
+    //when arrow key is released
     $(document).keyup(function (e) {
         if (e.keyCode == key_left){
             keyState[0] = false;
+            document.getElementById("LeftBtn").style.backgroundColor = "";
         }
         if (e.keyCode == key_forward){
             keyState[1] = false;
@@ -33,8 +42,8 @@ function initializeNavKeys() {
         }
         if (e.keyCode == key_right){
             keyState[2] = false;
+            document.getElementById("RightBtn").style.backgroundColor = "";
         }
-        
     });
 }
 
