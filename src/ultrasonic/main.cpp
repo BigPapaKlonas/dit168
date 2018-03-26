c7a7ff0efae46c08a57a551c98ec7ca77727ed54
Erik Laurin <guslauer@student.gu.se>
2018-03-26 11:41:09 +0200

    migrated C into CPP code and CMakeLists Co-authored-by: justasAtGU <gusstirju@student.gu.se>

---
+++ src/ultrasonic/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+extern "C"
+{
+#include <rc_usefulincludes.h>
+#include <roboticscape.h>
+}
+
+#define BUS                             1
+#define DEV_ADDRESS_ULTRASONIC_FRONT    0x71
+
+// Read
+#define SOFTWARE_REVISION               0x00
+#define FIRST_ECHO_LOW_BYTE_REGISTER    0x03
+
+// Write
+#define COMMAND_REGISTER                0x00
+
+// Units
+#define CENTIMETER                      0x51
+
+int main() {
+
+    uint8_t reading_byte = 0;
+    uint8_t version_byte = 0;
+
+    // Initialize cape library
+    std::cout << "Initialize roboticscape library" << std::endl;
+    if (rc_initialize()) {
+        std::cout << "ERROR: failed to initialize rc_initialize(), are you root?\n" << std::endl;
+        return -1;
+    }
+
+
+    // Initialize bus where ultrasonics are located
+    std::cout << "Initialize bus" << std::endl;
+    rc_i2c_init(BUS, DEV_ADDRESS_ULTRASONIC_FRONT);
+
+    while (1) {
+
+        //rc_i2c_claim_bus(BUS);
+
+        // Triggers ultrasonic sensor to store a centimeter reading in its register
+        rc_i2c_write_byte(BUS, COMMAND_REGISTER, CENTIMETER);
+
+        // Preventing deadlocks
+        using namespace std::literals::chrono_literals;
+        std::this_thread::sleep_for(0.005s);
+
+        // Loops while the sensor is ranging
+        rc_i2c_read_byte(BUS, SOFTWARE_REVISION, &version_byte);
+        while (version_byte != 11) {
+            rc_i2c_read_byte(BUS, SOFTWARE_REVISION, &version_byte);
+        }
+
+        // Loops while the sensor is reading
+        rc_i2c_read_byte(BUS, FIRST_ECHO_LOW_BYTE_REGISTER, &reading_byte);
+        printf("Reading: %u cm\n", reading_byte);
+
+        // Preventing deadlocks
+        std::this_thread::sleep_for(0.01s);
+    }
+
+    // exit cleanly
+    //roboticscape::rc_i2c_release_bus(BUS);
+    rc_cleanup();
+    return 0;
+}
