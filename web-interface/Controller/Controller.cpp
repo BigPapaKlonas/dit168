c6c39172506d33bc281e0eb143e312b20c4bda3d
Erik Laurin <guslauer@student.gu.se>
2018-04-09 10:36:45 +0200

    fixed incorrectly commit, correct code added

--- web-interface/Controller/Controller.cpp
+++ web-interface/Controller/Controller.cpp
@@ -1,72 +1,133 @@
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#include <emscripten/bind.h>
+using namespace emscripten;
+#endif
+
 #include <cstdint>
 #include <chrono>
 #include <iostream>
 #include <sstream>
 #include <thread>
 
-#include "cluon/OD4Session.hpp"
-#include "cluon/Envelope.hpp"
+#include "cluon-complete.hpp"
+#include "build/messages.hpp"
 
-#include "messages.hpp"
 
-int main(int /*argc*/, char** /*argv*/) {
+class Example {
+public:
+    // Default Constructor
+    Example() {
 
-    cluon::OD4Session od4(111,[](cluon::data::Envelope &&envelope) noexcept {
-        if (envelope.dataType() == opendlv::proxy::GroundSteeringReading::ID()) {
-            opendlv::proxy::GroundSteeringReading receivedMsg = cluon::extractMessage<opendlv::proxy::GroundSteeringReading>(std::move(envelope));
-            std::cout << "Sent a message for ground steering to " << receivedMsg.steeringAngle() << "." << std::endl;
+        /*
+        cluon::OD4Session od4(111, [](cluon::data::Envelope &&envelope) noexcept {});
+
+        if (od4.isRunning() == 0) {
+            std::cout << "ERROR: No OD4 is running!" << std::endl;
+            exit(0);
         }
-        else if (envelope.dataType() == opendlv::proxy::PedalPositionReading::ID()) {
-            opendlv::proxy::PedalPositionReading receivedMsg = cluon::extractMessage<opendlv::proxy::PedalPositionReading>(std::move(envelope));
-            std::cout << "Sent a message for pedal position to " << receivedMsg.percent() << "." << std::endl;
-        }
-    });
-
-    if(od4.isRunning() == 0)
-    {
-        std::cout << "ERROR: No od4 running!!!" << std::endl;
-        return -1;
+         */
     }
 
-    std::cout << "Hello World again!" << std::endl;
+    void forward(void);
 
+    void stop(void);
+
+    void left(void);
+
+    void right(void);
+
+    void setSpeed(float requestedSpeed);
+
+    void setAngle(float requestedAngle);
+
+private:
+    float speed;
+    float angle;
     opendlv::proxy::GroundSteeringReading msgSteering;
     opendlv::proxy::PedalPositionReading msgPedal;
+};
 
-    const int delay = 1000;
+static cluon::OD4Session od4(111, [](cluon::data::Envelope &&envelope) noexcept {});
 
-    std::cout << "Now move forward ..." << std::endl;
+void Example::forward() {
+    std::cout << "Forward" << std::endl;
     msgSteering.steeringAngle(0.0);
     od4.send(msgSteering);
-    msgPedal.percent(0.4);
+    msgPedal.percent(speed);
     od4.send(msgPedal);
-    std::this_thread::sleep_for(std::chrono::milliseconds(2 * delay));
-
-    std::cout << "Now turn left a little bit ..." << std::endl;
-    msgPedal.percent(0.2);
-    od4.send(msgPedal);
-    msgSteering.steeringAngle(-15.0);
-    od4.send(msgSteering);
-    std::this_thread::sleep_for(std::chrono::milliseconds(delay));
-
-    std::cout << "Now turn right a little bit ..." << std::endl;
-    msgSteering.steeringAngle(15.0);
-    od4.send(msgSteering);
-    std::this_thread::sleep_for(std::chrono::milliseconds(delay));
-
-    std::cout << "Now move forward again..." << std::endl;
-    msgSteering.steeringAngle(0.0);
-    od4.send(msgSteering);
-    msgPedal.percent(0.4);
-    od4.send(msgPedal);
-    std::this_thread::sleep_for(std::chrono::milliseconds(delay));
-
-    std::cout << "Now Stop ..." << std::endl;
-    msgPedal.percent(0.0);
-    od4.send(msgPedal);
-    std::this_thread::sleep_for(std::chrono::milliseconds(delay));
-
-
-    return 0;
 }
 
+void Example::stop() {
+    std::cout << "Stop" << std::endl;
+    msgSteering.steeringAngle(0.0);
+    od4.send(msgSteering);
+    msgPedal.percent(0.0);
+    od4.send(msgPedal);
+}
+
+void Example::left() {
+    std::cout << "Left" << std::endl;
+    msgSteering.steeringAngle(angle);
+    od4.send(msgSteering);
+    msgPedal.percent(speed);
+    od4.send(msgPedal);
+}
+
+void Example::right() {
+    std::cout << "Right" << std::endl;
+    msgSteering.steeringAngle(-angle);
+    od4.send(msgSteering);
+    msgPedal.percent(speed);
+    od4.send(msgPedal);
+}
+
+void Example::setAngle(float requestedAngle) {
+    angle = requestedAngle;
+}
+
+void Example::setSpeed(float requestedSpeed) {
+    speed = requestedSpeed;
+}
+
+int main(int argc, char **argv) {
+
+    // TEST MAIN
+
+    Example e;
+
+    e.setSpeed(0.16);
+    e.forward();
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    e.stop();
+
+    e.setSpeed(0.5);
+    e.forward();
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+    e.right();
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+    e.left();
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+    /* ACTUAL MAIN
+    (void) argc;
+    (void) argv;
+    return 0;
+
+     **/
+}
+
+
+#ifdef __EMSCRIPTEN__
+EMSCRIPTEN_BINDINGS(libcluon) {
+    function("forward", &forward);
+    function("left", &left);
+    function("right", &right);
+    function("stop", &stop);
+    function("setSpeed", &setSpeed);
+    function("setAngle", &setAngle);
+
+}
+#endif
