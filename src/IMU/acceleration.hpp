c3974de8eb42fc70fd2ff9b16cafad1bb6ebd0cb
Isabelle Törnqvist <gustornqis@student.gu.se>
2018-04-30 11:02:02 +0200

    formatting according to vera++, included in Cmake. RIP tabs

--- src/IMU/acceleration.hpp
+++ src/IMU/acceleration.hpp
@@ -2,11 +2,12 @@
 #define ACCELERATION
 #include <cstdint>
 
-class acceleration {
-public:
-	float getAcceleration(float x_accel, float y_accel);
-	float getDistanceTraveled(float accel, float time, float speed);
-	float getSpeed(float accel, float initialVelocity);
-	float reduceAccelNoice(float samples[]);
-};
+class acceleration
+ {
+ public:
+  float getAcceleration(float x_accel, float y_accel);
+  float getDistanceTraveled(float accel, float time, float speed);
+  float getSpeed(float accel, float initialVelocity);
+  float reduceAccelNoice(float samples[]);
+ };
 #endif
